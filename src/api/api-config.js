5e38e3433b76b7af914bb07fdee6d1e813d3e53c
Samuel Chen <sam_chen4@outlook.com>
2024-05-28 14:54:36 -0600

    allow https to http

--- src/api/api-config.js
+++ src/api/api-config.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
+// const baseURL = process.env.REACT_APP_PROXY || 'http://34.218.254.87:5000';
 const baseURL = process.env.REACT_APP_PROXY || 'http://localhost:5000';
 // creates a custom instance of axios
 const api = axios.create({
