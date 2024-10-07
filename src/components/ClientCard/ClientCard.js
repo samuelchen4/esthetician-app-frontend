10152a4b2d1634acd22a9b7442423dcd7e1c9ce6
Samuel Chen <samuel.chen@consumergenius.com>
2024-10-07 15:11:13 -0600

    added triple dots component to indicate that you can swipe the photos

--- src/components/ClientCard/ClientCard.js
+++ src/components/ClientCard/ClientCard.js
@@ -1,8 +1,6 @@
 import React from "react";
-import CircleImage from "src/components/CircleImage";
 import Carousel from "src/components/Carousel";
-import { Button } from "src/components/ui/button";
-import { Dot, Heart, Star, StarHalf } from "lucide-react";
+import { Dot, Heart, Star, StarHalf, Ellipsis } from "lucide-react";
 
 const images = [
   "/static/nails-1.png",
@@ -38,32 +36,28 @@ const ClientCard = ({
           className="px-0 py-0 rounded-t-lg"
           //   imageClassName="border-2 rounded-lg border-black"
         />
-        {/* <CircleImage
-          size={60}
-          src="/static/client-card-profile-picture-2.png"
-          className="absolute bottom-[-10px] left-4 border-2 border-black shadow-sm"
-        /> */}
+        {/* <p className="z-10 absolute -bottom-6 right-3 border border-gray-400 p-1.5 rounded-full shadow-sm bg-white text-black">
+          <MoveRight size="20" />
+        </p> */}
       </div>
       <div className=" relative mx-4 mt-5 mb-4 grow">
         <img
           src="/static/client-card-profile-picture-2.png"
           className="absolute top-[-70px] h-14 border-2 border-black rounded-sm shadow-md object-cover"
         />
-        {/* <CircleImage
-          size={70}
-          src="/static/client-card-profile-picture-2.png"
-          className="absolute top-[-70px] border-2 border-black shadow-sm"
-        /> */}
-        <h5 className="mb-0.5 font-bold text-base ">
+        <Ellipsis
+          size="24"
+          className="absolute -top-3 left-1/2 transform -translate-x-1/2 text-gray-400"
+        />
+        <h5 className="mb-0.5 font-bold text-lg ">
           {firstName} {lastName}
         </h5>
-        <div className="flex flex-col ">
+        <div className="flex flex-col">
           <div className="flex items-center">
-            <p className="text-blue-500">
+            <p className="text-primary font-semibold">
               {services !== null ? services.join(", ") : "Specialist"}
             </p>
             <Dot size="20" />
-
             <div className=" flex items-center">
               <Star size="12" className="text-yellow-400" />
               <Star size="12" className="text-yellow-400" />
@@ -73,7 +67,7 @@ const ClientCard = ({
             </div>
           </div>
           <p>{!location ? "Calgary, AB" : location}</p>
-          {/* {schedules.length > 0 && <p>Avaliability: {schedules.join(" ")}</p>} */}
+          {/* <p>Avaliable: Monday, Wednesday, Friday</p> */}
         </div>
         {/* <div className="absolute bottom-0 right-0 flex items-center">
           <Star size="12" className="text-yellow-400" />
